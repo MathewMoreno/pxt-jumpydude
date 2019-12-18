5e5ecb096b526fbd74417817fea760c4b3a7c2d0
MathewMoreno <58610701+MathewMoreno@users.noreply.github.com>
2019-12-18 13:47:16 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,37 +3,40 @@ namespace SpriteKind {
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Object, function (sprite, otherSprite) {
     info.changeScoreBy(1)
-    Hank.ay = -50
     Trampoline.setKind(SpriteKind.Object)
-    Trampoline.x += Math.randomRange(-1, 1)
-    Trampoline.y += -50
+    Jared.vy = -110
+    Trampoline.x += Math.randomRange(Math.randomRange(-30, -50), Math.randomRange(30, 50))
+    Trampoline.y += -75
 })
-sprites.onCreated(SpriteKind.Object, function (sprite) {
-	
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false, effects.starField)
+})
+info.onCountdownEnd(function () {
+    game.over(true, effects.confetti)
 })
 let Trampoline: Sprite = null
-let Hank: Sprite = null
-scene.setBackgroundColor(5)
-Hank = sprites.create(img`
-. . . e e e e e e e e e e e e . 
-. . e e e e e e e e e e e e e e 
-. e e e e e e e e e e e e e e e 
-. e e e 4 4 4 4 4 e e e e e e . 
-. e e 4 4 4 4 4 4 4 4 4 4 . . . 
-. e 4 4 4 4 4 4 4 f f 4 4 4 . . 
-. 4 4 4 4 4 4 4 4 f f 4 4 4 . . 
-. 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-. . 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-. . 4 4 4 4 4 4 4 4 4 4 4 . . . 
-. . . 4 4 4 4 4 4 4 4 4 . . . . 
-. . . . . . 4 4 4 4 4 . . . . . 
-. . . . . 8 8 8 8 8 8 8 . . . . 
-. . . 8 8 8 8 8 8 8 8 8 8 . . . 
-. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
-. 8 8 2 8 8 2 8 8 2 8 2 2 2 8 8 
+let Jared: Sprite = null
+scene.setBackgroundColor(12)
+Jared = sprites.create(img`
+. . . . . . . . . . e e e e e . 
+. . . . . . . e e e e e e e e e 
+. . . . e e e e e e e e e e e . 
+. . . e e e e e e e e e . . . . 
+. . . e e 3 3 3 3 3 3 3 3 . . . 
+. . . 3 3 3 5 3 3 5 3 3 3 . . . 
+. . . 3 3 3 3 3 3 3 3 3 3 . . . 
+. . . 3 3 3 3 5 5 3 3 3 3 . . . 
+. . . 3 3 5 5 5 5 5 5 3 3 . . . 
+. . . . 3 3 5 3 3 5 3 3 . . . . 
+. . . . . . 3 3 3 3 3 . . . . . 
+. . 8 8 8 8 8 8 8 8 8 8 . . . . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+8 8 2 8 8 2 8 8 8 2 8 8 8 8 8 . 
+8 8 2 8 8 2 8 8 8 8 8 8 8 8 8 8 
+8 8 2 2 2 2 8 8 8 2 8 8 8 8 8 8 
 `, SpriteKind.Player)
-Hank.ay = 25
-scene.cameraFollowSprite(Hank)
+scene.cameraFollowSprite(Jared)
+Jared.ay = 55
 Trampoline = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -41,22 +44,77 @@ Trampoline = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
+. c c b b b b b b b b b b c c . 
+. c c 1 f 1 f 1 f 1 f 1 f c c . 
+. c c f 1 f 1 f 1 f 1 f 1 c c . 
+. c c b b b b b b b b b b c c . 
+. c c . . . . . . . . . . c c . 
+. c c . . . . . . . . . . c c . 
+. c c . . . . . . . . . . c c . 
 . . . . . . . . . . . . . . . . 
-. . . b b b b b b b b b b . . . 
-. . b f d f d f d f d f d b . . 
-. b b d f d f d f d f d f b b . 
-. b b f d f d f d f d f d b b . 
-. b b b b b b b b b b b b b b . 
-. b b . . . . . . . . . . b b . 
-. b b . . . . . . . . . . b b . 
-. b b . . . . . . . . . . b b . 
+. . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Object)
-Hank.setPosition(300, 500)
+Jared.setPosition(300, 450)
 Trampoline.setPosition(300, 520)
+let EvilDude = sprites.create(img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f 2 f f f f f f f 2 f f f f 
+f f f 2 2 f f f f f 2 2 f f f f 
+f f f 2 2 2 2 2 2 2 2 2 f f f f 
+f f 2 2 2 2 2 2 2 2 2 2 2 f f f 
+f f 2 f f f 2 2 f f f 2 2 f f f 
+f 2 2 2 f 2 2 2 2 f 2 2 2 f f f 
+f 2 2 2 f 2 2 2 2 f 2 2 2 f f f 
+f f 2 2 2 2 2 2 2 2 2 2 2 f f f 
+f f 2 2 2 2 2 2 f f 2 2 2 f f f 
+f f f 2 f f f f f 2 2 2 f f f f 
+f f f 2 2 2 2 2 2 2 2 f f f f 2 
+f f f f f 2 2 2 2 f f f f f f 2 
+f 2 2 2 2 2 2 2 2 2 2 2 2 f 2 2 
+`, SpriteKind.Enemy)
+EvilDude.setPosition(300, 670)
+EvilDude.follow(Jared, 90)
+let RocketPack = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f f . . . 
+. . . . 4 4 e b b b b e 4 4 . . 
+. . . . 4 4 b e b b e b 4 4 . . 
+. . . . 4 4 b e b b e b 4 4 . . 
+. . . . 4 4 b e b b e b 4 4 . . 
+. . . . 4 4 e b b b b e 4 4 . . 
+. . . . 4 4 f b b b b f 4 4 . . 
+. . . . 2 2 . f f f f . 2 2 . . 
+. . . . 2 2 . . . . . . 2 2 . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+RocketPack.setPosition(300, 670)
+scene.setTile(8, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 4 4 4 4 4 4 4 4 4 4 4 2 2 2 
+2 4 2 2 4 4 4 4 4 4 4 4 2 2 4 2 
+2 4 4 2 2 4 4 4 4 4 2 2 4 4 4 2 
+2 4 4 4 2 4 4 4 4 2 4 4 4 4 4 2 
+2 4 4 4 4 2 4 2 2 4 4 4 4 4 4 2 
+2 4 4 4 4 4 2 2 4 4 4 4 4 4 4 2 
+2 4 4 4 4 4 2 2 4 4 4 4 4 4 4 2 
+2 4 4 4 4 2 2 2 2 4 4 4 4 4 4 2 
+2 4 4 4 2 2 4 4 2 2 4 4 4 4 4 2 
+2 4 4 2 2 4 4 4 4 2 2 4 4 4 4 2 
+2 4 2 2 4 4 4 4 4 4 2 2 4 4 4 2 
+2 4 2 4 4 4 4 4 4 4 4 2 2 4 4 2 
+2 2 2 4 4 4 4 4 4 4 4 4 2 2 4 2 
+2 2 4 4 4 4 4 4 4 4 4 4 4 4 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, true)
+info.startCountdown(60)
 game.onUpdate(function () {
-    Hank.x += controller.dx()
-})
-game.onUpdateInterval(5000, function () {
-    Hank.ay = 50
+    Jared.x += controller.dx()
 })
